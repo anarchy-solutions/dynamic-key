e1c3543ca2fa6358af1156d234b8dafe646cccdd
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2024-11-10 17:25:25 +0100

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,1 +1,5 @@
-
+module.exports = (req, res) => {
+    const key = Math.random().toString(36).substring(2, 10);
+    res.setHeader("Cache-Control", "no-store"); 
+    res.status(200).json({ key }); 
+};
